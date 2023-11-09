2aa5771abcb7d36c6c81ff14694d627db957a38e
Valerii Mironchenko <vmironchenko@provectus.com>
2023-11-09 18:55:23 +0200

    Add tests for pgvector feature

--- odd-collector/tests/integration/helpers.py
+++ odd-collector/tests/integration/helpers.py
@@ -1,6 +1,6 @@
 from funcy import filter, lfilter
 from odd_models import DataEntity
-from odd_models.models import DataEntityList, DataEntityType
+from odd_models.models import DataEntityList, DataEntityType, DataSetField
 
 
 def find_by_type(
@@ -17,5 +17,14 @@ def find_by_name(data_entity_list: DataEntityList, name: str) -> DataEntity:
         filter(
             lambda data_entity: data_entity.name.lower() == name.lower(),
             data_entity_list.items,
+        )
+    )
+
+
+def find_dataset_field_by_name(dataset_field_list: list[DataSetField], name: str) -> DataSetField:
+    return next(
+        filter(
+            lambda dataset_field: dataset_field.name.lower() == name.lower(),
+            dataset_field_list,
         )
     )
