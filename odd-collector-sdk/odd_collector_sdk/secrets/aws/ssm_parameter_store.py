7830dea285bae6c18a7222d4a74ba2f3867bc493
Valerii Mironchenko <vmironchenko@provectus.com>
2023-12-14 13:10:32 +0200

    Reduce complexity of secret backend config. Update AWSSystemsManagerParameterStoreBackend and config examples

--- odd-collector-sdk/odd_collector_sdk/secrets/aws/ssm_parameter_store.py
+++ odd-collector-sdk/odd_collector_sdk/secrets/aws/ssm_parameter_store.py
@@ -7,47 +7,54 @@ from yaml import safe_load
 class AWSSystemsManagerParameterStoreBackend(BaseSecretsBackend):
     def __init__(self, **kwargs) -> None:
         super().__init__()
-        self._region_name = kwargs.get("region_name", "us-east-1")
-        self._collector_id = kwargs.get("collector_id", "")
-        self._config_prefix = kwargs.get("config_prefix", "/odd/collector_config")
-        self._collector_settings_section_prefix = kwargs.get(
-            "collector_settings_section_prefix", "/collector_settings"
+
+        collector_settings_parameter_name = kwargs.get(
+            "collector_settings_parameter_name", "/odd/collector_config/collector_settings"
         )
-        self._plugins_section_prefix = kwargs.get("plugins_section_prefix", "/plugins")
+        collector_plugins_prefix = kwargs.get(
+            "collector_plugins_prefix", "/odd/collector_config/plugins"
+        )
+
+        self._region_name = kwargs.get("region_name", "us-east-1")
+        self._collector_settings_parameter_name = self._ensure_leading_slash(
+            collector_settings_parameter_name
+        )
+        self._collector_plugins_prefix = self._ensure_leading_slash(collector_plugins_prefix)
         self._ssm_client = boto3.client("ssm", region_name=self._region_name)
 
     @staticmethod
     def _ensure_leading_slash(secret_name: str) -> str:
         """
         In AWS SSM Parameter Store leading "/" is a mandatory naming convention.
-        Function ensures path starts with "/", and can be applied for validation of path sections.
+        Function ensures naming path starts with "/".
 
         Parameters:
             secret_name: secret parameter name.
         """
         return secret_name if secret_name.startswith("/") else f"/{secret_name}"
 
-    @property
-    def _base_secret_prefix(self) -> str:
-        config_prefix = self._ensure_leading_slash(self._config_prefix)
-        collector_id = (
-            self._ensure_leading_slash(self._collector_id) if self._collector_id else ""
-        )
-        return f"{config_prefix}{collector_id}"
+    def _get_secret_by_name(self, name: str, decrypt: bool = True) -> dict:
+        """
+        Retrieve a secret from AWS Systems Manager Parameter Store by name.
 
-    @property
-    def _collector_settings_prefix(self) -> str:
-        section_prefix = self._ensure_leading_slash(
-            self._collector_settings_section_prefix
-        )
-        return f"{self._base_secret_prefix}{section_prefix}"
+        Parameters:
+            name: the name of the parameter.
+            decrypt: whether to decrypt the secret if it's encrypted (default is True).
 
-    @property
-    def _plugins_prefix(self) -> str:
-        section_prefix = self._ensure_leading_slash(self._plugins_section_prefix)
-        return f"{self._base_secret_prefix}{section_prefix}"
+        Returns:
+            A dictionary containing information about the secret.
+            The dictionary may include keys like 'Name', 'Type', 'Value', etc.
+        """
+        try:
+            response = self._ssm_client.get_parameter(Name=name, WithDecryption=decrypt)
+            secret = response.get("Parameter", {})
+            return secret
+        except self._ssm_client.exceptions.ParameterNotFound as e:
+            # Handle the case when the specified parameter doesn't exist
+            logger.info(f"ParameterNotFound: {e}")
+            return {}
 
-    def _get_secrets_with_prefix(self, prefix: str, decrypt: bool = True) -> list[dict]:
+    def _get_secrets_by_prefix(self, prefix: str, decrypt: bool = True) -> list[dict]:
         """
         Retrieve all secrets from AWS Systems Manager Parameter Store
         whose names start with the given prefix.
@@ -73,18 +80,15 @@ class AWSSystemsManagerParameterStoreBackend(BaseSecretsBackend):
 
     def get_collector_settings(self) -> dict:
         """
-        Unpack directly the name of secrets and thier values from raw ssm_client response
-        getting the following result (example): {'platform_host_url': '', 'token': ''}
+        Unpack directly the secret values from raw ssm_client response getting
+        the following result (example): {'platform_host_url': '', 'token': ''}
         """
-        collector_settings = self._get_secrets_with_prefix(
-            self._collector_settings_prefix
+        collector_settings = self._get_secret_by_name(
+            self._collector_settings_parameter_name
         )
 
         if collector_settings:
-            result = {
-                cs["Name"].rsplit("/", 1)[1]: cs["Value"] for cs in collector_settings
-            }
-            return result
+            return safe_load(collector_settings["Value"])
         return {}
 
     def get_plugins(self) -> list[dict]:
@@ -92,9 +96,8 @@ class AWSSystemsManagerParameterStoreBackend(BaseSecretsBackend):
         Unpack directly the secret values from raw ssm_client response getting
         the following result (example): [{'type': '', 'name': '', 'host': ''}, ...]
         """
-        plugins = self._get_secrets_with_prefix(self._plugins_prefix)
+        plugins = self._get_secrets_by_prefix(self._collector_plugins_prefix)
 
         if plugins:
-            result = [safe_load(p["Value"]) for p in plugins]
-            return result
+            return [safe_load(p["Value"]) for p in plugins]
         return []
