63e7cf8ed02671f40ac22ce849046ab89a479b43
Valerii Mironchenko <vmironchenko@provectus.com>
2024-01-22 13:36:17 +0200

    Refactor Adapter with DRY for new get_relationship_list() method realisation

--- odd-collector/odd_collector/adapters/postgresql/adapter.py
+++ odd-collector/odd_collector/adapters/postgresql/adapter.py
@@ -1,10 +1,10 @@
 from collections import defaultdict
 
-from odd_collector.adapters.postgresql.models import Table
+from odd_collector.adapters.postgresql.models import Table, Schema, Relationship
 from odd_collector.domain.plugin import PostgreSQLPlugin
 from odd_collector_sdk.domain.adapter import BaseAdapter
 from odd_models import DataEntity
-from odd_models.models import DataEntityList
+from odd_models.models import DataEntityList, RelationshipList
 from oddrn_generator import PostgresqlGenerator
 
 from .logger import logger
@@ -21,51 +21,64 @@ class Adapter(BaseAdapter):
     def __init__(self, config: PostgreSQLPlugin) -> None:
         super().__init__(config)
 
+    def _create_repository(self) -> PostgreSQLRepository:
+        return PostgreSQLRepository(
+            ConnectionParams.from_config(self.config), self.config.schemas_filter
+        )
+
+    def _get_tables(self) -> list[Table]:
+        with self._create_repository() as repo:
+            return repo.get_tables()
+
+    def _get_schemas(self) -> list[Schema]:
+        with self._create_repository() as repo:
+            return repo.get_schemas()
+
+    def _get_relationships(self) -> list[Relationship]:
+        with self._create_repository() as repo:
+            return repo.get_relationships()
+
     def create_generator(self) -> PostgresqlGenerator:
         return PostgresqlGenerator(
             host_settings=self.config.host, databases=self.config.database
         )
 
     def get_data_entity_list(self) -> DataEntityList:
-        with PostgreSQLRepository(
-            ConnectionParams.from_config(self.config), self.config.schemas_filter
-        ) as repo:
-            schema_entities: list[DataEntity] = []
+        schema_entities: list[DataEntity] = []
 
-            all_table_entities: dict[str, DataEntity] = {}
+        all_table_entities: dict[str, DataEntity] = {}
 
-            tables = repo.get_tables()
-            schemas = repo.get_schemas()
-            relationships = repo.get_relationships()
+        self.generator.set_oddrn_paths(**{"databases": self.config.database})
 
-            self.generator.set_oddrn_paths(**{"databases": self.config.database})
+        tables_by_schema = defaultdict(list)
+        for table in self._get_tables():
+            tables_by_schema[table.table_schema].append(table)
 
-            tables_by_schema = defaultdict(list)
-            for table in tables:
-                tables_by_schema[table.table_schema].append(table)
-
-            for schema in schemas:
-                tables_per_schema: list[Table] = tables_by_schema.get(
-                    schema.schema_name, []
-                )
-                table_entities_tmp = map_tables(self.generator, tables_per_schema)
-                schema_entities.append(
-                    map_schema(
-                        self.generator, schema, list(table_entities_tmp.values())
-                    )
-                )
-                all_table_entities |= table_entities_tmp
-
-            database_entity = map_database(
-                self.generator, self.config.database, schema_entities
+        for schema in self._get_schemas():
+            tables_per_schema: list[Table] = tables_by_schema.get(
+                schema.schema_name, []
             )
-
-            create_lineage(tables, all_table_entities)
-
-            return DataEntityList(
-                data_source_oddrn=self.get_data_source_oddrn(),
-                items=[*all_table_entities.values(), *schema_entities, database_entity],
+            table_entities_tmp = map_tables(self.generator, tables_per_schema)
+            schema_entities.append(
+                map_schema(
+                    self.generator, schema, list(table_entities_tmp.values())
+                )
             )
+            all_table_entities |= table_entities_tmp
+
+        database_entity = map_database(
+            self.generator, self.config.database, schema_entities
+        )
+
+        create_lineage(self._get_tables(), all_table_entities)
+
+        return DataEntityList(
+            data_source_oddrn=self.get_data_source_oddrn(),
+            items=[*all_table_entities.values(), *schema_entities, database_entity],
+        )
+
+    def get_relationship_list(self) -> RelationshipList:
+        pass
 
 
 def create_lineage(tables: list[Table], data_entities: dict[str, DataEntity]) -> None:
