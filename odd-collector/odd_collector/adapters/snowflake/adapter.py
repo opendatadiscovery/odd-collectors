19c86dee50d7a71079a3db8728e8b9522d4d1b17
Valerii Mironchenko <vmironchenko@provectus.com>
2024-02-16 12:14:35 +0200

    Refactor. Decomposing adapter logic. Fix client behavior when for each data retrieving the new connection is created. Add retrieving of imported keys.

--- odd-collector/odd_collector/adapters/snowflake/adapter.py
+++ odd-collector/odd_collector/adapters/snowflake/adapter.py
@@ -1,4 +1,5 @@
 from typing import List, Optional, Tuple, Type
+from functools import cached_property
 
 from odd_collector.domain.plugin import SnowflakePlugin
 from odd_collector_sdk.domain.adapter import BaseAdapter
@@ -7,7 +8,7 @@ from odd_models.models import DataEntity, DataEntityList
 from oddrn_generator import Generator, SnowflakeGenerator
 
 from .client import SnowflakeClient, SnowflakeClientBase
-from .domain import Pipe, Table, View
+from .domain import Pipe, Table, View, RawPipe, RawStage, ImportedKey
 from .map import map_database, map_pipe, map_schemas, map_table, map_view
 
 
@@ -15,15 +16,10 @@ class Adapter(BaseAdapter):
     config: SnowflakePlugin
     generator: SnowflakeGenerator
 
-    def __init__(
-        self,
-        config: SnowflakePlugin,
-        client: Optional[Type[SnowflakeClientBase]] = SnowflakeClient,
-    ):
+    def __init__(self, config: SnowflakePlugin) -> None:
         self._database_name = config.database.upper()
-        self._client = client(config)
-
         super().__init__(config)
+        self._metadata = self._get_database_metadata()
 
     def create_generator(self) -> Generator:
         return SnowflakeGenerator(
@@ -31,11 +27,36 @@ class Adapter(BaseAdapter):
             databases=self._database_name,
         )
 
-    def get_data_entity_list(self) -> DataEntityList:
-        raw_pipes = self._client.get_raw_pipes()
-        raw_stages = self._client.get_raw_stages()
+    def _get_database_metadata(self) -> dict[str, list]:
+        with SnowflakeClient(self.config) as client:
+            return {
+                "tables": client.get_tables(),
+                "raw_pipes": client.get_raw_pipes(),
+                "raw_stages": client.get_raw_stages(),
+                "imported_keys": client.get_imported_keys(),
+            }
+
+    @cached_property
+    def _tables(self) -> list[Table]:
+        return self._metadata["tables"]
+
+    @cached_property
+    def _raw_pipes(self) -> list[RawPipe]:
+        return self._metadata["raw_pipes"]
+
+    @cached_property
+    def _raw_stages(self) -> list[RawStage]:
+        return self._metadata["raw_stages"]
+
+
+    @cached_property
+    def _imported_keys(self) -> list[ImportedKey]:
+        return self._metadata["imported_keys"]
+
+    @cached_property
+    def _pipe_entities(self) -> list[DataEntity]:
         pipes: List[Pipe] = []
-        for raw_pipe in raw_pipes:
+        for raw_pipe in self._raw_pipes:
             pipes.extend(
                 Pipe(
                     name=raw_pipe.pipe_name,
@@ -44,32 +65,34 @@ class Adapter(BaseAdapter):
                     stage_type=raw_stage.stage_type,
                     downstream=raw_pipe.downstream,
                 )
-                for raw_stage in raw_stages
+                for raw_stage in self._raw_stages
                 if raw_pipe.stage_full_name == raw_stage.stage_full_name
             )
-        pipes_entities = [map_pipe(pipe, self.generator) for pipe in pipes]
-        tables = self._client.get_tables()
+        return [map_pipe(pipe, self.generator) for pipe in pipes]
 
-        tables_with_data_entities: List[
-            Tuple[Table, DataEntity]
-        ] = self._get_tables_entities(tables)
+    @cached_property
+    def _table_entities(self) -> list[tuple[Table, DataEntity]]:
+        return self._get_tables_entities(self._tables)
 
+    @cached_property
+    def _schema_entities(self) -> list[DataEntity]:
+        return self._get_schemas_entities(self._table_entities)
+
+    @cached_property
+    def _database_entity(self) -> DataEntity:
+        return self._get_database_entity(self._schema_entities)
+
+    def get_data_entity_list(self) -> DataEntityList:
         try:
-            tables_entities = [
-                table_with_entity[1] for table_with_entity in tables_with_data_entities
-            ]
-            schemas_entities = self._get_schemas_entities(tables_with_data_entities)
-            database_entity: DataEntity = self._get_database_entity(schemas_entities)
-
-            all_entities = [
-                *tables_entities,
-                *schemas_entities,
-                database_entity,
-                *pipes_entities,
-            ]
-
+            imported_keys = self._metadata["imported_keys"]
             return DataEntityList(
-                data_source_oddrn=self.get_data_source_oddrn(), items=all_entities
+                data_source_oddrn=self.get_data_source_oddrn(),
+                items=[
+                    *[te[1] for te in self._table_entities],
+                    *self._schema_entities,
+                    self._database_entity,
+                    *self._pipe_entities,
+                ]
             )
         except Exception as e:
             raise MappingDataError("Error during mapping") from e
--- odd-collector/odd_collector/adapters/snowflake/client.py
+++ odd-collector/odd_collector/adapters/snowflake/client.py
@@ -1,8 +1,9 @@
 import contextlib
 import re
+from ast import literal_eval
 from abc import ABC, abstractmethod
 from collections import defaultdict
-from typing import Callable, Dict, List, Type, Union
+from typing import Callable, Dict, List, Type, Union, Any
 
 from funcy import lsplit
 from odd_collector.domain.plugin import SnowflakePlugin
@@ -11,183 +12,201 @@ from odd_collector_sdk.errors import DataSourceError
 from snowflake import connector
 from snowflake.connector.cursor import DictCursor
 
-from .domain import Column, Pipe, RawPipe, RawStage, Table, View
+from .domain import Column, Pipe, RawPipe, RawStage, Table, View, ImportedKey
 from .logger import logger
 
 TABLES_VIEWS_QUERY = """
-with recursive cte as (
+    with recursive cte as (
+        select
+            referenced_database,
+            referenced_schema,
+            referenced_object_name,
+            referenced_object_id,
+            referenced_object_domain,
+            referencing_database,
+            referencing_schema,
+            referencing_object_name,
+            referencing_object_id,
+            referencing_object_domain
+        from snowflake.account_usage.object_dependencies
+        union all
+        select
+            deps.referenced_database,
+            deps.referenced_schema,
+            deps.referenced_object_name,
+            deps.referenced_object_id,
+            deps.referenced_object_domain,
+            deps.referencing_database,
+            deps.referencing_schema,
+            deps.referencing_object_name,
+            deps.referencing_object_id,
+            deps.referencing_object_domain
+        from snowflake.account_usage.object_dependencies deps
+        join cte
+            on deps.referencing_object_id = cte.referenced_object_id
+            and deps.referencing_object_domain = cte.referenced_object_domain
+    ),
+    upstream as (
+        select
+            referencing_database as node_database,
+            referencing_schema as node_schema,
+            referencing_object_name as node_name,
+            referencing_object_domain as node_domain,
+            array_agg(
+                distinct concat_ws(
+                    '.',
+                    referenced_database,
+                    referenced_schema,
+                    referenced_object_name,
+                    referenced_object_domain
+                )
+            ) as nodes
+        from cte
+        group by
+            referencing_database,
+            referencing_schema,
+            referencing_object_name,
+            referencing_object_id,
+            referencing_object_domain
+    ),
+    downstream as (
+        select
+            referenced_database as node_database,
+            referenced_schema as node_schema,
+            referenced_object_name as node_name,
+            referenced_object_domain as node_domain,
+            array_agg(
+                distinct concat_ws(
+                    '.',
+                    referencing_database,
+                    referencing_schema,
+                    referencing_object_name,
+                    referencing_object_domain
+                )
+            ) as nodes
+        from cte
+        group by referenced_database,
+            referenced_schema,
+            referenced_object_name,
+            referenced_object_id,
+            referenced_object_domain
+    )
     select
-        referenced_database,
-        referenced_schema,
-        referenced_object_name,
-        referenced_object_id,
-        referenced_object_domain,
-        referencing_database,
-        referencing_schema,
-        referencing_object_name,
-        referencing_object_id,
-        referencing_object_domain
-    from snowflake.account_usage.object_dependencies
-    union all
-    select
-        deps.referenced_database,
-        deps.referenced_schema,
-        deps.referenced_object_name,
-        deps.referenced_object_id,
-        deps.referenced_object_domain,
-        deps.referencing_database,
-        deps.referencing_schema,
-        deps.referencing_object_name,
-        deps.referencing_object_id,
-        deps.referencing_object_domain
-    from snowflake.account_usage.object_dependencies deps
-    join cte
-        on deps.referencing_object_id = cte.referenced_object_id
-        and deps.referencing_object_domain = cte.referenced_object_domain
-),
-upstream as (
-    select
-        referencing_database as node_database,
-        referencing_schema as node_schema,
-        referencing_object_name as node_name,
-        referencing_object_domain as node_domain,
-        array_agg(
-            distinct concat_ws(
-                '.',
-                referenced_database,
-                referenced_schema,
-                referenced_object_name,
-                referenced_object_domain
-            )
-        ) as nodes
-    from cte
-    group by
-        referencing_database,
-        referencing_schema,
-        referencing_object_name,
-        referencing_object_id,
-        referencing_object_domain
-),
-downstream as (
-    select
-        referenced_database as node_database,
-        referenced_schema as node_schema,
-        referenced_object_name as node_name,
-        referenced_object_domain as node_domain,
-        array_agg(
-            distinct concat_ws(
-                '.',
-                referencing_database,
-                referencing_schema,
-                referencing_object_name,
-                referencing_object_domain
-            )
-        ) as nodes
-    from cte
-    group by referenced_database,
-        referenced_schema,
-        referenced_object_name,
-        referenced_object_id,
-        referenced_object_domain
-)
-select
-    t.table_catalog,
-    t.table_schema,
-    t.table_name,
-    t.table_owner,
-    t.table_type,
-    t.is_transient,
-    t.clustering_key,
-    t.row_count,
-    t.bytes,
-    t.retention_time,
-    t.self_referencing_column_name,
-    t.reference_generation,
-    t.user_defined_type_catalog,
-    t.user_defined_type_schema,
-    t.user_defined_type_name,
-    t.is_insertable_into,
-    t.is_typed,
-    t.created,
-    t.last_altered,
-    t.auto_clustering_on,
-    t.comment as table_comment,
-    v.comment as view_comment,
-    v.view_definition,
-    v.is_secure,
-    v.is_updatable,
-    array_to_string(u.nodes, ',') as upstream,
-    array_to_string(d.nodes, ',') as downstream
-from information_schema.tables t
-left join information_schema.views as v
-    on v.table_catalog = t.table_catalog
-    and v.table_schema = t.table_schema
-    and v.table_name = t.table_name
-left join upstream u
-    on u.node_database = t.table_catalog
-    and u.node_schema = t.table_schema
-    and u.node_name = t.table_name
-left join downstream d
-    on d.node_database = t.table_catalog
-    and d.node_schema = t.table_schema
-    and d.node_name = t.table_name
-where t.table_schema != 'INFORMATION_SCHEMA'
-order by table_catalog, table_schema, table_name
-
+        t.table_catalog,
+        t.table_schema,
+        t.table_name,
+        t.table_owner,
+        t.table_type,
+        t.is_transient,
+        t.clustering_key,
+        t.row_count,
+        t.bytes,
+        t.retention_time,
+        t.self_referencing_column_name,
+        t.reference_generation,
+        t.user_defined_type_catalog,
+        t.user_defined_type_schema,
+        t.user_defined_type_name,
+        t.is_insertable_into,
+        t.is_typed,
+        t.created,
+        t.last_altered,
+        t.auto_clustering_on,
+        t.comment as table_comment,
+        v.comment as view_comment,
+        v.view_definition,
+        v.is_secure,
+        v.is_updatable,
+        array_to_string(u.nodes, ',') as upstream,
+        array_to_string(d.nodes, ',') as downstream
+    from information_schema.tables t
+    left join information_schema.views as v
+        on v.table_catalog = t.table_catalog
+        and v.table_schema = t.table_schema
+        and v.table_name = t.table_name
+    left join upstream u
+        on u.node_database = t.table_catalog
+        and u.node_schema = t.table_schema
+        and u.node_name = t.table_name
+    left join downstream d
+        on d.node_database = t.table_catalog
+        and d.node_schema = t.table_schema
+        and d.node_name = t.table_name
+    where t.table_schema != 'INFORMATION_SCHEMA'
+    order by table_catalog, table_schema, table_name;
 """
 
 COLUMNS_QUERY = """
-select
-   c.table_catalog,
-   c.table_schema,
-   c.table_name,
-   c.column_name,
-   c.ordinal_position,
-   c.column_default,
-   c.is_nullable,
-   c.data_type,
-   c.character_maximum_length,
-   c.character_octet_length,
-   c.numeric_precision,
-   c.numeric_precision_radix,
-   c.numeric_scale,
-   c.collation_name,
-   c.is_identity,
-   c.identity_generation,
-   c.identity_start,
-   c.identity_increment,
-   c.identity_cycle,
-   c.comment
-from information_schema.columns as c
-join information_schema.tables as t
-    on c.table_catalog = t.table_catalog
-    and c.table_schema = t.table_schema
-    and c.table_name = t.table_name
-where c.table_schema != 'INFORMATION_SCHEMA'
-order by
-    c.table_catalog,
-    c.table_schema,
-    c.table_name,
-    c.ordinal_position
+    select
+       c.table_catalog,
+       c.table_schema,
+       c.table_name,
+       c.column_name,
+       c.ordinal_position,
+       c.column_default,
+       c.is_nullable,
+       c.data_type,
+       c.character_maximum_length,
+       c.character_octet_length,
+       c.numeric_precision,
+       c.numeric_precision_radix,
+       c.numeric_scale,
+       c.collation_name,
+       c.is_identity,
+       c.identity_generation,
+       c.identity_start,
+       c.identity_increment,
+       c.identity_cycle,
+       c.comment
+    from information_schema.columns as c
+    join information_schema.tables as t
+        on c.table_catalog = t.table_catalog
+        and c.table_schema = t.table_schema
+        and c.table_name = t.table_name
+    where c.table_schema != 'INFORMATION_SCHEMA'
+    order by
+        c.table_catalog,
+        c.table_schema,
+        c.table_name,
+        c.ordinal_position;
 """
 
 RAW_PIPES_QUERY = """
-
-SELECT PIPE_CATALOG, PIPE_SCHEMA, PIPE_NAME, DEFINITION
-FROM INFORMATION_SCHEMA.PIPES
-;
-
+    SELECT PIPE_CATALOG, PIPE_SCHEMA, PIPE_NAME, DEFINITION
+    FROM INFORMATION_SCHEMA.PIPES;
 """
 
 RAW_STAGES_QUERY = """
-
-SELECT STAGE_NAME, STAGE_CATALOG, STAGE_SCHEMA, STAGE_URL, STAGE_TYPE
-FROM INFORMATION_SCHEMA.STAGES
-;
-
+    SELECT STAGE_NAME, STAGE_CATALOG, STAGE_SCHEMA, STAGE_URL, STAGE_TYPE
+    FROM INFORMATION_SCHEMA.STAGES;
 """
 
-PRIMARY_KEYS_QUERY = "SHOW PRIMARY KEYS IN DATABASE"
+PRIMARY_KEYS_QUERY = """
+    SHOW PRIMARY KEYS IN DATABASE;
+"""
+
+IMPORTED_KEYS_QUERIES = (
+    '''SHOW IMPORTED KEYS IN DATABASE;''',
+    '''
+        SELECT
+            "created_on",
+            "pk_database_name",
+            "pk_schema_name",
+            "pk_table_name",
+            ARRAY_AGG("pk_column_name") WITHIN GROUP (ORDER BY "key_sequence") AS "pk_columns",
+            "fk_database_name",
+            "fk_schema_name",
+            "fk_table_name",
+            ARRAY_AGG("fk_column_name") WITHIN GROUP (ORDER BY "key_sequence") AS "fk_columns",
+            "fk_name",
+            "pk_name"
+        FROM TABLE(RESULT_SCAN(LAST_QUERY_ID()))
+        GROUP BY
+            "created_on",
+            "pk_database_name", "pk_schema_name", "pk_table_name", "pk_name",
+            "fk_database_name", "fk_schema_name", "fk_table_name", "fk_name";
+    '''
+)
 
 
 class SnowflakeClientBase(ABC):
@@ -195,44 +214,45 @@ class SnowflakeClientBase(ABC):
         self._config = config
 
     @abstractmethod
-    def get_tables(self) -> List[Table]:
+    def get_tables(self) -> list[Table]:
         raise NotImplementedError
 
     @abstractmethod
-    def get_raw_pipes(self) -> List[RawPipe]:
+    def get_raw_pipes(self) -> list[RawPipe]:
         raise NotImplementedError
 
     @abstractmethod
-    def get_raw_stages(self) -> List[RawStage]:
+    def get_raw_stages(self) -> list[RawStage]:
         raise NotImplementedError
 
 
 class SnowflakeClient(SnowflakeClientBase):
-    @contextlib.contextmanager
-    def connect(self):
-        cursor = None
-        connection = None
+    def __init__(self, config):
+        super().__init__(config)
+        self._conn = None
+
+    def __enter__(self):
         try:
-            connection = connector.connect(
+            logger.info("Setting up Snowflake connection...")
+            self._conn = connector.connect(
                 user=self._config.user,
                 password=self._config.password.get_secret_value(),
                 account=self._config.account,
                 database=self._config.database,
                 warehouse=self._config.warehouse,
             )
-            cursor = DictCursor(connection)
-            yield cursor
         except Exception as e:
             raise DataSourceError(
                 f"Error during getting information from Snowflake. {e}"
             ) from e
-        finally:
-            if cursor:
-                cursor.close()
-            if connection:
-                connection.close()
+        return self
 
-    def get_tables(self) -> List[Union[Table, View]]:
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self._conn:
+            logger.info("Snowflake connection has been closed.")
+            self._conn.close()
+
+    def get_tables(self) -> list[Union[Table, View]]:
         logger.info("Getting tables and views from Snowflake")
 
         def is_belongs(table: Table) -> Callable[[Column], bool]:
@@ -245,11 +265,11 @@ class SnowflakeClient(SnowflakeClientBase):
 
             return _
 
-        with self.connect() as cursor:
-            tables: List[Table] = self._fetch_tables(cursor)
-            columns: List[Column] = self._fetch_columns(cursor)
-            primary_keys: Dict[str, List] = self._fetch_primary_keys(cursor)
-            clustering_keys: Dict[str, List] = self._get_clustering_keys(tables)
+        with DictCursor(self._conn) as cursor:
+            tables: list[Table] = self._fetch_tables(cursor)
+            columns: list[Column] = self._fetch_columns(cursor)
+            primary_keys: dict[str, list] = self._fetch_primary_keys(cursor)
+            clustering_keys: dict[str, list] = self._get_clustering_keys(tables)
 
             for column in columns:
                 if column.column_name in primary_keys.get(column.table_name, []):
@@ -265,18 +285,24 @@ class SnowflakeClient(SnowflakeClientBase):
                 columns = not_belongs
             return tables
 
-    def get_raw_pipes(self) -> List[RawPipe]:
+    def get_raw_pipes(self) -> list[RawPipe]:
         logger.info("Getting pipes from Snowflake")
-        with self.connect() as cursor:
-            return self._fetch_something(RAW_PIPES_QUERY, cursor, RawPipe)
+        with DictCursor(self._conn) as cursor:
+            return self._base_fetch_entity_list(RAW_PIPES_QUERY, cursor, RawPipe)
 
-    def get_raw_stages(self) -> List[RawStage]:
+    def get_raw_stages(self) -> list[RawStage]:
         logger.info("Getting stages from Snowflake")
-        with self.connect() as cursor:
-            return self._fetch_something(RAW_STAGES_QUERY, cursor, RawStage)
+        with DictCursor(self._conn) as cursor:
+            return self._base_fetch_entity_list(RAW_STAGES_QUERY, cursor, RawStage)
 
-    def _get_clustering_keys(self, tables: List[Table]) -> Dict[str, List]:
-        res: Dict[str, List] = {}
+    def get_imported_keys(self) -> list[ImportedKey]:
+        logger.info("Getting imported keys from Snowflake")
+        with DictCursor(self._conn) as cursor:
+            return self._fetch_something_many(IMPORTED_KEYS_QUERIES, cursor, ImportedKey)
+
+    @staticmethod
+    def _get_clustering_keys(tables: list[Table]) -> dict[str, list]:
+        res: dict[str, list] = {}
 
         # Snowflake clustering keys could look like: "LINEAR(to_date(post_timestamp))", "LINEAR(column2, column3)"
         # cl_keys matches any parentheses and everything inside them that do not contain any parentheses.
@@ -289,41 +315,55 @@ class SnowflakeClient(SnowflakeClientBase):
                     res[table.table_name] = matches.group("cl_keys").split(", ")
         return res
 
-    def _fetch_tables(self, cursor: DictCursor) -> List[Table]:
-        result: List[Table] = []
+    @staticmethod
+    def _fetch_tables(cursor: DictCursor) -> list[Table]:
+        result: list[Table] = []
 
         cursor.execute(TABLES_VIEWS_QUERY)
-
         for raw_object in cursor.fetchall():
             if raw_object.get("TABLE_TYPE") == "BASE TABLE":
                 result.append(Table.parse_obj(LowerKeyDict(raw_object)))
             elif raw_object.get("TABLE_TYPE") == "VIEW":
                 result.append(View.parse_obj(LowerKeyDict(raw_object)))
-
         return result
 
-    def _fetch_primary_keys(self, cursor: DictCursor) -> Dict[str, List]:
-        cursor.execute(PRIMARY_KEYS_QUERY)
+    def _fetch_columns(self, cursor: DictCursor) -> list[Column]:
+        return self._base_fetch_entity_list(COLUMNS_QUERY, cursor, Column)
+
+    @staticmethod
+    def _fetch_primary_keys(cursor: DictCursor) -> dict[str, list]:
         res = defaultdict(list)
+
+        cursor.execute(PRIMARY_KEYS_QUERY)
         for pk in cursor.fetchall():
             res[pk["table_name"]].append(pk["column_name"])
         return res
 
     @staticmethod
-    def _fetch_something(
+    def _base_fetch_entity_list(
         query: str,
         cursor: DictCursor,
-        entity_type: Type[Union[Pipe, RawPipe, RawStage]],
-    ) -> List[Union[Pipe, RawPipe, RawStage]]:
-        result: List[entity_type] = []
+        entity_type: Any,
+    ) -> list[Any]:
         cursor.execute(query)
+        return [
+            entity_type.parse_obj(LowerKeyDict(raw_object))
+            for raw_object in cursor.fetchall()
+        ]
+
+    @staticmethod
+    def _fetch_something_many(
+        queries: tuple,
+        cursor: DictCursor,
+        entity_type: Type[Union[Pipe, RawPipe, RawStage, ImportedKey]],
+    ) -> list[Union[Pipe, RawPipe, RawStage, ImportedKey]]:
+        result: list[entity_type] = []
+        for query in queries:
+            cursor.execute(query)
         for raw_object in cursor.fetchall():
+            for col in ("pk_columns", "fk_columns"):
+                raw_object[col] = raw_object[col].strip(
+                    "[]"
+                ).replace("\n", "").replace(" ", "").replace('"', '').split(",")
             result.append(entity_type.parse_obj(LowerKeyDict(raw_object)))
         return result
-
-    def _fetch_columns(self, cursor: DictCursor) -> List[Column]:
-        cursor.execute(COLUMNS_QUERY)
-        return [
-            Column.parse_obj(LowerKeyDict(raw_column))
-            for raw_column in cursor.fetchall()
-        ]
