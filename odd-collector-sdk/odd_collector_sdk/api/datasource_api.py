e88900ebfe29a0a5fa2e39362671fe50145a9db5
Valerii Mironchenko <vmironchenko@provectus.com>
2024-07-30 14:13:06 +0300

    Remove redundant HttpClient class + add platform response handling

--- odd-collector-sdk/odd_collector_sdk/api/datasource_api.py
+++ odd-collector-sdk/odd_collector_sdk/api/datasource_api.py
@@ -1,49 +1,78 @@
+import json
 import sys
 from datetime import timedelta
 from timeit import default_timer as timer
 
+from aiohttp import ClientSession, ClientTimeout, TCPConnector
 from odd_models.models import DataEntityList, DataSourceList
 
-from ..errors import IngestionDataError, PlatformApiError, RegisterDataSourceError
 from ..logger import logger
-from .http_client import HttpClient
 
 
 class PlatformApi:
-    def __init__(self, http_client: HttpClient, platform_url: str) -> None:
-        self.__client = http_client
-        self.__platform_url = platform_url
+    def __init__(
+        self,
+        token: str,
+        platform_url: str,
+        connection_timeout_seconds: int = 300,
+        verify_ssl: bool = False,
+    ) -> None:
+        self.platform_url = platform_url
+        self.headers = {
+            "content-type": "application/json",
+            "Authorization": f"Bearer {token}",
+        }
+        self.verify_ssl = verify_ssl
+        self.timeout = ClientTimeout(total=connection_timeout_seconds)
 
-    async def register_datasource(self, requests: DataSourceList):
-        try:
-            return await self.__client.post(
-                f"{self.__platform_url}/ingestion/datasources", requests.json()
-            )
-        except PlatformApiError as e:
-            raise RegisterDataSourceError(e.response, requests) from e
-        except Exception:
-            raise
+    async def register_datasource(self, data_source_list: DataSourceList):
+        async with ClientSession(
+            connector=TCPConnector(verify_ssl=self.verify_ssl),
+            timeout=self.timeout,
+        ) as session:
+            try:
+                response = await session.post(
+                    url=f"{self.platform_url}/ingestion/datasources",
+                    data=data_source_list.json(),
+                    headers=self.headers,
+                )
+                response.raise_for_status()
+                return response
+            except Exception as e:
+                raise Exception(
+                    f"Platform response: {self._get_platform_response_content(response)}.\n Exception message: {str(e)}"
+                )
 
     async def ingest_data(self, data_entity_list: DataEntityList):
-        try:
-            json_start = timer()
-            data = data_entity_list.json()
-            logger.debug(
-                f"Encoded size: {sys.getsizeof(data)/(1024*1024):.3f} MB, took: {timedelta(seconds=timer()  - json_start )}"
-            )
+        async with ClientSession(
+            connector=TCPConnector(verify_ssl=self.verify_ssl),
+            timeout=self.timeout,
+        ) as session:
+            try:
+                json_start = timer()
+                data = data_entity_list.json()
+                logger.debug(
+                    f"Encoded size: {sys.getsizeof(data)/(1024*1024):.3f} MB, took: {timedelta(seconds=timer() - json_start)}"
+                )
 
-            ingest_start = timer()
-            response = await self.__client.post(
-                f"{self.__platform_url}/ingestion/entities",
-                data,
-            )
-            response.raise_for_status()
-            ingest_end = timer()
-            logger.debug(
-                f"Ingestion to platform took {timedelta(seconds=ingest_end  - ingest_start )}"
-            )
-            return response
-        except PlatformApiError as e:
-            raise IngestionDataError(e.response, data_entity_list) from e
-        except Exception:
-            raise
+                ingest_start = timer()
+                response = await session.post(
+                    url=f"{self.platform_url}/ingestion/entities",
+                    data=data,
+                    headers=self.headers,
+                )
+                response.raise_for_status()
+                ingest_end = timer()
+
+                logger.debug(
+                    f"Ingestion to platform took {timedelta(seconds=ingest_end - ingest_start)}"
+                )
+                return response
+            except Exception as e:
+                raise Exception(
+                    f"Platform response: {self._get_platform_response_content(response)}.\n Exception message: {str(e)}"
+                )
+
+    @staticmethod
+    def _get_platform_response_content(response):
+        return json.loads(str(response.content._buffer[0].decode("utf-8")))
